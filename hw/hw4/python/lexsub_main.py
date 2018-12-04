b51cf3a1cd0ca3a0419bc8ef8b44668aaa58bbd5
boygao1992 <wg2313@columbia.edu>
2018-12-04 16:26:32 -0500

    hw4 2/6

--- hw/hw4/python/lexsub_main.py
+++ hw/hw4/python/lexsub_main.py
@@ -8,6 +8,7 @@ from nltk.corpus import wordnet as wn
 from nltk.corpus import stopwords
 import gensim
 import numpy as np
+from collections import defaultdict
 
 # Participate in the 4705 lexical substitution competition (optional): NO
 # Alias: [please invent some name]
@@ -16,10 +17,21 @@ def tokenize(s):
     s = "".join(" " if x in string.punctuation else x for x in s.lower())
     return s.split()
 
+def underscore2space(str):
+    return str.replace('_', ' ')
+
+def space2underscore(str):
+    return str.replace(' ', '_')
+
 def get_candidates(lemma, pos):
     # Part 1
-    possible_synonyms = []
-    return possible_synonyms
+    possible_synonyms = set()
+    for lemma1 in wn.lemmas(space2underscore(lemma), pos):
+        for lemma2 in lemma1.synset().lemmas():
+            possible_synonyms.add(lemma2.name())
+    possible_synonyms.remove(space2underscore(lemma))
+    return list(map(underscore2space, possible_synonyms))
+
 
 def smurf_predictor(context):
     """
@@ -28,7 +40,16 @@ def smurf_predictor(context):
     return 'smurf'
 
 def wn_frequency_predictor(context):
-    return None # replace for part 2
+    lemma = space2underscore(context.lemma)
+    pos = context.pos
+    cooccurrence = defaultdict(int)
+    for lemma1 in wn.lemmas(lemma, pos):
+        count = lemma1.count()
+        for lemma2 in lemma1.synset().lemmas():
+            cooccurrence[lemma2.name()] += count
+    if lemma in cooccurrence:
+        del cooccurrence[lemma]
+    return underscore2space(max(cooccurrence, key=cooccurrence.get)) # replace for part 2
 
 def wn_simple_lesk_predictor(context):
     return None #replace for part 3
@@ -53,5 +74,6 @@ if __name__=="__main__":
 
     for context in read_lexsub_xml(sys.argv[1]):
         #print(context)  # useful for debugging
-        prediction = smurf_predictor(context)
+        # prediction = smurf_predictor(context)
+        prediction = wn_frequency_predictor(context)
         print("{}.{} {} :: {}".format(context.lemma, context.pos, context.cid, prediction))
